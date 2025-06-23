375df36be60d90b9ba37edb82cf238dc0de70c45
Eva Raste <rasteeva3003@gmail.com>
2025-06-23 12:12:28 +0530

    Update settings.py

--- aichatbot_project/settings.py
+++ aichatbot_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jt4%peku9g*a4j0n7^&@-gv7jq=%e*a&y^id9ny@7%3)m3^8b-'
+#SECRET_KEY = 'django-insecure-jt4%peku9g*a4j0n7^&@-gv7jq=%e*a&y^id9ny@7%3)m3^8b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
